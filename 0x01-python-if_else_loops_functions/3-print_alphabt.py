7287b8848685074a35848fd2a37f6cf5e733e4cf
Noutcha <jonathan@OFTY.fr>
2023-01-04 09:23:59 +0100

    all correction done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(97,123):
-	if chr(i) != "q" and chr(i) != "e":
-		print(chr(i), end ="")
-
-
+for i in range(97, 123):
+ if i != 113 and i != 101:
+  print("{:c}".format(i), end ="")
